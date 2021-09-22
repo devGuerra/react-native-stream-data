9121253f2c4020ba356a7e481606767172cda34f
Roberto Guerra <prosaka@hotmail.com>
2021-09-21 23:04:07 -0300

    :sparkles: Finish Login Twitch

--- src/hooks/useAuth.tsx
+++ src/hooks/useAuth.tsx
@@ -1,8 +1,16 @@
-import { makeRedirectUri, revokeAsync, startAsync } from 'expo-auth-session';
-import React, { useEffect, createContext, useContext, useState, ReactNode } from 'react';
-import { generateRandom } from 'expo-auth-session/build/PKCE';
+import { makeRedirectUri, revokeAsync, startAsync } from "expo-auth-session";
+import React, {
+  useEffect,
+  createContext,
+  useContext,
+  useState,
+  ReactNode,
+} from "react";
+import { generateRandom } from "expo-auth-session/build/PKCE";
 
-import { api } from '../services/api';
+const { CLIENT_ID } = process.env;
+
+import { api } from "../services/api";
 
 interface User {
   id: number;
@@ -26,78 +34,111 @@ interface AuthProviderData {
 const AuthContext = createContext({} as AuthContextData);
 
 const twitchEndpoints = {
-  authorization: 'https://id.twitch.tv/oauth2/authorize',
-  revocation: 'https://id.twitch.tv/oauth2/revoke'
+  authorization: "https://id.twitch.tv/oauth2/authorize",
+  revocation: "https://id.twitch.tv/oauth2/revoke",
 };
 
 function AuthProvider({ children }: AuthProviderData) {
   const [isLoggingOut, setIsLoggingOut] = useState(false);
   const [isLoggingIn, setIsLoggingIn] = useState(false);
   const [user, setUser] = useState({} as User);
-  const [userToken, setUserToken] = useState('');
+  const [userToken, setUserToken] = useState("");
 
   // get CLIENT_ID from environment variables
+  useEffect(() => {
+    api.defaults.headers["Client-Id"] = CLIENT_ID;
+  }, []);
 
   async function signIn() {
     try {
       // set isLoggingIn to true
-
+      setIsLoggingIn(true);
       // REDIRECT_URI - create OAuth redirect URI using makeRedirectUri() with "useProxy" option set to true
+      const REDIRECT_URI = makeRedirectUri({ useProxy: true });
       // RESPONSE_TYPE - set to "token"
+      const RESPONSE_TYPE = "token";
       // SCOPE - create a space-separated list of the following scopes: "openid", "user:read:email" and "user:read:follows"
+      const SCOPE = encodeURI("openid user:read:email user:read:follows");
       // FORCE_VERIFY - set to true
+      const FORCE_VERIFY = true;
       // STATE - generate random 30-length string using generateRandom() with "size" set to 30
-
-      // assemble authUrl with twitchEndpoint authorization, client_id, 
+      const STATE = generateRandom(30);
+      // assemble authUrl with twitchEndpoint authorization, client_id,
       // redirect_uri, response_type, scope, force_verify and state
-
+      const authUrl =
+        twitchEndpoints.authorization +
+        `?client_id=${CLIENT_ID}` +
+        `&redirect_uri=${REDIRECT_URI}` +
+        `&response_type=${RESPONSE_TYPE}` +
+        `&scope=${SCOPE}` +
+        `&force_verify=${FORCE_VERIFY}` +
+        `&state=${STATE}`;
       // call startAsync with authUrl
+      const authResponse = await startAsync({ authUrl });
 
       // verify if startAsync response.type equals "success" and response.params.error differs from "access_denied"
-      // if true, do the following:
-
-        // verify if startAsync response.params.state differs from STATE
+      if (
+        authResponse.type === "success" &&
+        authResponse.params.error !== "acess_denied"
+      ) {
         // if true, do the following:
+        // verify if startAsync response.params.state differs from STATE
+        if (authResponse.params.state !== STATE) {
+          // if true, do the following:
           // throw an error with message "Invalid state value"
+          throw new Error("Invalid state value");
+        }
+      }
+      // add access_token to request's authorization header
+      api.defaults.headers.authorization = `Bearer ${authResponse.params.access_token}`;
+      // call Twitch API's users route
+      const userResponse = await api.get("/users");
 
-        // add access_token to request's authorization header
+      const user = userResponse.data.data[0];
 
-        // call Twitch API's users route
-
-        // set user state with response from Twitch API's route "/users"
-        // set userToken state with response's access_token from startAsync
+      setUserToken(authResponse.params.access_token);
+      setUser(user);
+      // set user state with response from Twitch API's route "/users"
+      // set userToken state with response's access_token from startAsync
     } catch (error) {
       // throw an error
+      throw new Error();
     } finally {
       // set isLoggingIn to false
+      setIsLoggingIn(false);
     }
   }
 
   async function signOut() {
     try {
       // set isLoggingOut to true
-
+      setIsLoggingIn(true);
       // call revokeAsync with access_token, client_id and twitchEndpoint revocation
+      revokeAsync(
+        {
+          token: userToken,
+          clientId: CLIENT_ID,
+        },
+        {
+          revocationEndpoint: twitchEndpoints.revocation,
+        }
+      );
     } catch (error) {
     } finally {
-      // set user state to an empty User object
-      // set userToken state to an empty string
-
-      // remove "access_token" from request's authorization header
-
-      // set isLoggingOut to false
+      setUser({} as User);
+      setUserToken("");
+      delete api.defaults.headers.authorization;
+      setIsLoggingIn(false);
     }
   }
 
-  useEffect(() => {
-    // add client_id to request's "Client-Id" header
-  }, [])
-
   return (
-    <AuthContext.Provider value={{ user, isLoggingOut, isLoggingIn, signIn, signOut }}>
-      { children }
+    <AuthContext.Provider
+      value={{ user, isLoggingOut, isLoggingIn, signIn, signOut }}
+    >
+      {children}
     </AuthContext.Provider>
-  )
+  );
 }
 
 function useAuth() {
--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,34 +1,37 @@
-import React from 'react';
-import { useTheme } from 'styled-components';
-import { Fontisto } from '@expo/vector-icons'
-import { ActivityIndicator, Alert, Modal, View } from 'react-native';
+import React from "react";
+import { useTheme } from "styled-components";
+import { Fontisto } from "@expo/vector-icons";
+import { ActivityIndicator, Alert, Modal, View } from "react-native";
 
-import { useAuth } from '../../hooks/useAuth';
+import { useAuth } from "../../hooks/useAuth";
 
-import LoginBannerImg from '../../assets/images/login.svg';
-import LogoImg from '../../assets/images/logo.svg';
+import LoginBannerImg from "../../assets/images/login.svg";
+import LogoImg from "../../assets/images/logo.svg";
 
-import { 
+import {
   Container,
   Content,
-  LoginBanner, 
-  LoginInfo, 
-  Header, 
-  Partner, 
-  Description, 
+  LoginBanner,
+  LoginInfo,
+  Header,
+  Partner,
+  Description,
   SignInButton,
   SignInButtonIcon,
-  SignInButtonText 
-} from './styles';
+  SignInButtonText,
+} from "./styles";
 
 export function SignIn() {
   const { signIn, isLoggingIn } = useAuth();
   const theme = useTheme();
 
-  // creates a function to handle sign in
-    // try to call and wait signIn
-    // if fails, display an Alert with the title "Erro SignIn" and message "Ocorreu um erro ao tentar logar no app"
-
+  async function handleSignIn() {
+    try {
+      await signIn();
+    } catch (error) {
+      Alert.alert("Erro SignIn", "Ocorreu um erro ao tentar logar no app");
+    }
+  }
   return (
     <Container
       from={{
@@ -56,36 +59,38 @@ export function SignIn() {
           </Header>
 
           <Description>
-            Veja dados{'\n'}
-            interessantes sobre{'\n'}
-            o mundo da Twitch
+            Veja dados{"\n"}
+            interessantes sobre{"\n"}o mundo da Twitch
           </Description>
 
-          {/* <SignInButton onPress={}>
+          <SignInButton onPress={signIn}>
             <SignInButtonIcon>
-              Verify if isLoggingIn is true
-              If it is, show an ActivityIndicator
-              Otherwise, show Fontisto's twitch icon
+              {isLoggingIn ? (
+                <ActivityIndicator size={20} color={theme.colors.white} />
+              ) : (
+                <Fontisto
+                  name="twitch"
+                  size={20}
+                  color={theme.colors.white}
+                  style={{ marginRight: 1 }}
+                />
+              )}
             </SignInButtonIcon>
 
             <SignInButtonText>
-              Verify if isLoggingIn is true
-              If it is, show "Entrando..."
-              Otherwise, show "Entrar com Twitch"
+              {isLoggingIn ? "Entrando..." : "Twitch"}
             </SignInButtonText>
-          </SignInButton> */}
+          </SignInButton>
         </LoginInfo>
       </Content>
 
-      <Modal 
+      <Modal
         animationType="fade"
         visible={isLoggingIn}
         statusBarTranslucent
         transparent
       >
-        <View
-          style={{ flex: 1, backgroundColor: 'rgba(14, 14, 16, 0.5)' }}
-        />
+        <View style={{ flex: 1, backgroundColor: "rgba(14, 14, 16, 0.5)" }} />
       </Modal>
     </Container>
   );
